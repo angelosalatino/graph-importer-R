4e97886a7a203bf9c0a67da94de510f5489d9214
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2017-03-22 17:24:23 +0000

    Update ExportImportGraph.R
    it should be capable of working with graphs made of only edges, not named vertex and so on.

--- ExportImportGraph.R
+++ ExportImportGraph.R
@@ -2,7 +2,7 @@
 #' library(jsonlite)
 
 
-#' Allows to both export and convert an igraph object to JSON.
+#' Allow to both export and convert an igraph object to JSON.
 #' 
 #' @param g the igraph object
 #' @param filename the out filename where to store the converted json. This parameter is optional. The funcion returns a json anyway. If this parameter is set it will also be saved into the file
@@ -12,8 +12,12 @@ exportGraph <- function(g,filename){
   graph <- list()
   graph$edges <- as_data_frame(g, what = "edges")
   graph$vertices <- as_data_frame(g, what="vertices")
+  # polish vertices
   row.names(graph$vertices) <- NULL
+  if(ncol(graph$vertices)==0) graph$vertices <- NULL #in case the 
+  
   graph$directed <- is.directed(g)
+  graph$name <- g$name
   #convert list into a json
   json.content <- toJSON(graph, pretty=TRUE)
   #write json into a file
@@ -32,8 +36,16 @@ exportGraph <- function(g,filename){
 #' @param filename either the json or the filename of the file containing the json
 importGraph <- function(filename){
   built.graph <- fromJSON(filename, flatten=TRUE)
-  built.g <- graph_from_data_frame(built.graph$edges, directed=built.graph$directed, vertices=built.graph$vertices)
+  if("vertices" %in% names(built.graph)){
+    built.g <- graph_from_data_frame(built.graph$edges, directed=built.graph$directed, vertices=built.graph$vertices)
+  }else{
+    built.g <- graph_from_data_frame(built.graph$edges, directed=built.graph$directed)
+  }
+  if("name" %in% names(built.graph)){
+    built.g$name <- built.graph$name
+  }
   return(built.g)
 }
+
 
 
